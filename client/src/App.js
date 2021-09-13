a84615e8dda55e6fbb327f9bc3280035d2eb9a0f
KamilMieniek <kamil.mieniek@gmail.com>
2021-09-13 22:41:09 +0200

    add create decks form

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,7 @@
-import React from "react";
-import FormComp from "./components/flash-card-form/form.component";
+import React from 'react';
+import FormComp from './components/flash-card-form/form.component';
 const App = () => {
   return (
-    // <div style={{background : "blue"}}>
-    //   <h1>Hello world</h1>
-    //   <ToDoList />
-    //   <Kamil>
-    //     <h3>Kamil component</h3>
-    //   </Kamil>
-    // </div>
     <div>
       <FormComp />
     </div>
