e4d6852f9ebb83b8782e658cabc1f19261e3821e
KamilMieniek <kamil.mieniek@gmail.com>
2021-10-19 23:46:53 +0200

    add header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './App.scss';
+import { Switch, Route } from 'react-router-dom';
 import FormComp from './components/flash-card-form/form.component';
+import Header from './components/header/header.component';
 const App = () => {
   return (
-    <div>
-      <FormComp />
+    <div className="app-container">
+      <Header />
+      <Switch>
+        <Route exact path="/page/form" component={FormComp} />
+      </Switch>
+      <p>gunwoo</p>
     </div>
   );
 };
